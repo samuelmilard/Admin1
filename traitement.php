d18c37e8373de99200711ca0f5f3181bcf1cdbc0
samuelmilard <sam.aloww@gmail.com>
2020-04-23 21:10:49 +0200

    ça marche enfiiiiiiiiiiiiiiiiiiiiiiiiin !!!

--- traitement.php
+++ traitement.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once('class_database.php');
-$connexion = new Database('db5000303646.hosting-data.io','dbs296633', 'dbu526590', '*w6kVB?2');
+require ('class_database.php');
+require ('User.php');
+$connexion = new database('db5000303646.hosting-data.io','dbs296633', 'dbu526590', '*w6kVB?2');
 $bdd = $connexion->PDOConnexion();
 
-require_once('User.php');
 
 
 $email = !empty($_POST['email']) ? $_POST['email'] : NULL;
